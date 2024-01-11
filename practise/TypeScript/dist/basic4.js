25f44a3495a0d2ebbe8274d48a72337b3b5173a6
Vinuka_Vilhan <v.v.fernandopulle@gmail.com>
2024-01-11 20:53:02 +0530

    ok

--- practise/TypeScript/dist/basic4.js
+++ practise/TypeScript/dist/basic4.js
@@ -1,1 +1,23 @@
 "use strict";
+// Checks the count of characters in a string
+function charCounter(words) {
+    var set = {};
+    words = words.toLowerCase();
+    // Store current time as `start`
+    let start = new Date().getTime();
+    for (let char of words) {
+        if (char.match(/[a-z0-9]/i)) {
+            if (set[char]) {
+                set[char]++;
+            }
+            else {
+                set[char] = 1;
+            }
+        }
+    }
+    // Calculate elapsed time
+    let elapsed = new Date().getTime() - start;
+    console.log(`Execution time: ${elapsed} milliseconds`);
+    return set;
+}
+console.log(charCounter("hello I AM @ YOUR Hero"));
--- practise/TypeScript/src/basic4.ts
+++ practise/TypeScript/src/basic4.ts
@@ -1,1 +1,30 @@
-//checks the 
+// Checks the count of characters in a string
+function charCounter(words: string) {
+    var set: {[key:string]: number} = {};
+
+    words = words.toLowerCase();
+
+    // Store current time as `start`
+    let start = new Date().getTime();
+
+    for(let char of words)
+    {
+        if (char.match(/[a-z0-9]/i))
+        {
+            if(set[char])
+            {
+                set[char]++;
+            }
+            else
+            {
+                set[char]=1;
+            }
+        }
+    }
+    // Calculate elapsed time
+    let elapsed = new Date().getTime() - start;
+    console.log(`Execution time: ${elapsed} milliseconds`);
+    return set;
+}
+
+console.log(charCounter("hello I AM @ YOUR Hero"));
