7e57ec458ec425c92caea6b3235e327741ba5c98
VinukaVilhan <fernandopullevv.22@uom.lk>
2024-01-26 23:47:06 +0530

    new

---
+++ practise/JS/test/test.js
@@ -0,0 +1,13 @@
+var score = 60;
+var message='';
+ 
+switch(score) {
+    //compring the boolean result
+  case (score === 69):
+    message += 'Not bad!';
+    break;
+  case (100): 
+       message += 'Great, perfect score!';
+}
+
+console.log(message);
