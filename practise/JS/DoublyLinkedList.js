35eaadf840ee7b20206fc33f23e25e7455244fc3
VinukaVilhan <fernandopullevv.22@uom.lk>
2024-02-16 09:41:47 +0530

    changes

--- practise/JS/DoublyLinkedList.js
+++ practise/JS/DoublyLinkedList.js
@@ -33,13 +33,69 @@ class doublyLinkedList
             newNode.prev = this.tail;
             //set the tail to be newly created node
             this.tail = newNode;
-            this.length++;
+            
         }
+        this.length++;
         return this;
+    }
+
+    pop()
+    {
+        if(!this.head)
+        {
+            return undefined;
+        }
+
+        if(this.length === 1)
+        {
+            this.head = null;
+            this.tail = null;
+        }
+        else
+        {
+            var currentTail = this.tail;
+            //update the tail to be the previous node
+            this.tail = currentTail.prev;
+            //set the new tail next to be null
+            this.tail.next = null;
+            currentTail.prev = null;
+        }
+        this.length--;
+        return currentTail;
+    }
+
+    //pop method from the beginning of the list
+    shift()
+    {
+        if(!this.head)
+        {
+            return undefined;
+        }
+        if(this.length === 1)
+        {
+            this.head = null;
+            this.tail = null;
+        }
+        else
+        {
+            var currentHead = this.head;
+
+            //update the head to be the next of the old head
+            this.head = currentHead.next;
+
+            //set the head prev property to null
+            this.head.prev = null;
+            //set the old head next to be null
+            currentHead.next = null;
+        }
+
+        this.length--;
+        return currentHead;
     }
 }
 
 var list = new doublyLinkedList();
 list.push('yo')
 list.push('are')
+list.shift()
 console.log(list)
